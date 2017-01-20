75b51f91291eefa771c52a0862184e5d61c871ad
zemuche <zemuche@gmail.com>
2017-01-19 19:07:34 -0600

    fix binary search recursive function to handle invalid inputs

--- search.py
+++ search.py
@@ -2,26 +2,28 @@ def binsearchrecur(sequence, item, start=0, stop=None):
     if len(sequence) == 0:
         return False
     if stop is None:
-        stop = len(sequence)
+        stop = len(sequence) - 1
     if start == stop and item == sequence[start]:
             return start
+    if sequence[start] > item or sequence[stop] < item:
+        return False
     mid = (stop + start) // 2
     if item == sequence[mid]:
         return mid
     elif item < sequence[mid]:
-        return binsearchrecur(sequence, item, start, mid)
+        return binsearchrecur(sequence, item, start, mid - 1)
     else:
         return binsearchrecur(sequence, item, mid + 1, stop)
 
 
-def binsearchiter(sequence, item, return_index=True):
+def binsearchiter(sequence, item, start=0, stop=None, return_index=True):
     if len(sequence) == 0:
         return False
     found = False
-    start = 0
-    end = len(sequence) - 1
-    while not found and start < end:
-        mid = (start + end) // 2
+    if stop is None:
+        stop = len(sequence) - 1
+    while not found and start < stop:
+        mid = (start + stop) // 2
         if item == sequence[mid]:
             found = True
             if return_index is True:
@@ -29,7 +31,7 @@ def binsearchiter(sequence, item, return_index=True):
         elif item > sequence[mid]:
             start = mid + 1
         else:
-            end = mid
+            stop = mid
     return found
 
 
@@ -40,8 +42,20 @@ def linsearch(sequence, target, start=0, end=None):
         end = len(sequence) - 1
     for i, value in enumerate(sequence[start:end+1], start):
         if value == target:
-            break
+            return i
     else:
         return False
-    return i
 
+
+def main():
+    alist = [43, 5, 0, 3, 86, 45, -8, 12]
+    print(alist)
+    a = linsearch(alist, 3)
+    alist.sort()
+    print(alist)
+    b = binsearchrecur(alist, 98)
+    print(a, b)
+
+
+if __name__ == '__main__':
+    main()
